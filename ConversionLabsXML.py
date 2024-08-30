aec40f0e4e84d45b8d7951dde7e59f1fefb251b4
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-08-30 12:03:13 -0500

    se leen los datos del excel de laboratorios

--- ConversionLabsXML.py
+++ ConversionLabsXML.py
@@ -15,9 +15,38 @@ nlp.add_pipe('language_detector',last=True)
 
 
 #Create Element Tree root class
-upmprojects = ET.Element("upmprojects")
-upmprojects.set('xmlns',"v1.upmproject.pure.atira.dk")
-upmprojects.set('xmlns:ns2',"v3.commons.pure.atira.dk")
+result = ET.Element("result")
+result.set('xmlns:xsi',"http://www.w3.org/2001/XMLSchema-instance")
+result.set('xsi:schemaLocation',"http://localhost:8080/ws/api/524/xsd/schema1.xsd")
 
-#Upload the xlsx file for test
-print("hola mundo")
+#Read excel and convert to dataframe
+dataframe1 = pd.read_excel(r'C:\Users\DELL\OneDrive - Pontificia Universidad Javeriana\Gestión Monitores Perfiles\Infraestructura\Excel\Formato Infraestructura - Perfiles y Capacidades.xlsx', sheet_name='Laboratorios', dtype=object)
+
+#print(dataframe1.head())
+
+column = dataframe1.id_unico.unique()
+
+for uid in column:
+    #Create Element Tree root class
+    temp_lab = dataframe1[dataframe1["id_unico"]==uid]
+    temp_lab_id = str(uid)
+    temp_lab_type = temp_lab["Tipo"].values[0]
+    temp_lab_eng_name = temp_lab["Nombre Inglés"].values[0]
+    temp_lab_es_name = temp_lab["Nombre español"].values[0]
+    temp_lab_eng_description = temp_lab["Descripción Inglés"].values[0]
+    temp_lab_es_description = temp_lab["Descripción Español"].values[0]
+    temp_lab_address = temp_lab["Dirección"].values[0]
+    temp_lab_phone = temp_lab["Número de contacto"].values[0]
+    temp_lab_url = temp_lab["URL"].values[0]
+    temp_lab_person = temp_lab["Nombre encargado"].values[0]
+    temp_lab_created = temp_lab["Fecha creación Laboratorios"].values[0]
+    temp_lab_loan_type = temp_lab["Disponible para préstamo"].values[0]
+    temp_lab_keywords = temp_lab["Palabras clave"].values[0]
+    temp_lab_services = temp_lab["Servicios del Laboratorio"].values[0]
+    temp_lab_certifications = temp_lab["Certificaciones"].values[0]
+
+    #print the first 5 rows of the dataframe
+
+print(temp_lab.head())
+
+
